07e447875ecebe91b3676cbe68b79a266cddbb0b
Christopher Finch <cptfinch@gmail.com>
2023-09-05 14:59:27 +0100

    Update

--- WindowsAPIProject.cpp
+++ WindowsAPIProject.cpp
@@ -3,6 +3,7 @@
 
 #include "framework.h"
 #include "WindowsAPIProject.h"
+#include "spdlog/spdlog.h"
 
 #define MAX_LOADSTRING 100
 #define MIN_WINDOW_WIDTH 200;
@@ -20,44 +21,47 @@ LRESULT CALLBACK    WndProc( HWND, UINT, WPARAM, LPARAM );
 INT_PTR CALLBACK    About( HWND, UINT, WPARAM, LPARAM );
 
 int APIENTRY wWinMain( _In_ HINSTANCE hInstance,
-	_In_opt_ HINSTANCE hPrevInstance,
-	_In_ LPWSTR    lpCmdLine,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPWSTR    lpCmdLine,
 	_In_ int       nCmdShow )
 {
-	UNREFERENCED_PARAMETER( hPrevInstance );
-	UNREFERENCED_PARAMETER( lpCmdLine );
+    auto logger = spdlog::basic_logger_mt("basic_logger", "application.log");
+    logger->info("Application started.");
 
-	// TODO: Place code here.
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
 
-	// Initialize global strings
+    // Initialize global strings
 	LoadStringW( hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING );
 	LoadStringW( hInstance, IDC_WINDOWSAPIPROJECT, szWindowClass, MAX_LOADSTRING );
 	MyRegisterClass( hInstance );
 
-	// Perform application initialization:
+    // Perform application initialization:
  	if ( !InitInstance( hInstance, nCmdShow ) )
-	{
-		return FALSE;
-	}
+    {
+        logger->error("Application initialization failed.");
+        return FALSE;
+    }
+    logger->info("Application initialization succeeded.");
 
 	HACCEL hAccelTable = LoadAccelerators( hInstance, MAKEINTRESOURCE( IDC_WINDOWSAPIPROJECT ) );
 
-	MSG msg;
+    MSG msg;
 
-	// Main message loop:
-	while ( GetMessage( &msg, nullptr, 0, 0 ) )
-	{
+    // Main message loop:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
 		if ( !TranslateAccelerator( msg.hwnd, hAccelTable, &msg ) )
-		{
+        {
 			TranslateMessage( &msg );
 			DispatchMessage( &msg );
-		}
-	}
+        }
+    }
 
-	return (int)msg.wParam;
+    logger->info("Application exited.");
+
+    return (int)msg.wParam;
 }
-
-
 
 //
 //  FUNCTION: MyRegisterClass()
@@ -66,23 +70,26 @@ int APIENTRY wWinMain( _In_ HINSTANCE hInstance,
 //
 ATOM MyRegisterClass( HINSTANCE hInstance )
 {
-	WNDCLASSEXW wcex;
+    WNDCLASSEXW wcex;
 
 	wcex.cbSize = sizeof( WNDCLASSEX );
 
-	wcex.style = CS_HREDRAW | CS_VREDRAW;
-	wcex.lpfnWndProc = WndProc;
-	wcex.cbClsExtra = 0;
-	wcex.cbWndExtra = 0;
-	wcex.hInstance = hInstance;
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = WndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
 	wcex.hIcon = LoadIcon( hInstance, MAKEINTRESOURCE( IDI_WINDOWSAPIPROJECT ) );
 	wcex.hCursor = LoadCursor( nullptr, IDC_ARROW );
-	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
 	wcex.lpszMenuName = MAKEINTRESOURCEW( IDC_WINDOWSAPIPROJECT );
-	wcex.lpszClassName = szWindowClass;
+    wcex.lpszClassName = szWindowClass;
 	wcex.hIconSm = LoadIcon( wcex.hInstance, MAKEINTRESOURCE( IDI_SMALL ) );
 
-	return RegisterClassExW( &wcex );
+    auto logger = spdlog::get("basic_logger");
+    logger->info("Window class registered.");
+
+    return RegisterClassExW(&wcex);
 }
 
 //
@@ -97,20 +104,25 @@ ATOM MyRegisterClass( HINSTANCE hInstance )
 //
 BOOL InitInstance( HINSTANCE hInstance, int nCmdShow )
 {
-	hInst = hInstance; // Store instance handle in our global variable
+    hInst = hInstance; // Store instance handle in our global variable
 
 	HWND hWnd = CreateWindowW( szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
 		CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr );
 
 	if ( !hWnd )
-	{
-		return FALSE;
-	}
+    {
+        auto logger = spdlog::get("basic_logger");
+        logger->error("Window creation failed.");
+        return FALSE;
+    }
 
-	ShowWindow( hWnd, nCmdShow );
-	UpdateWindow( hWnd );
+    auto logger = spdlog::get("basic_logger");
+    logger->info("Window created successfully.");
 
-	return TRUE;
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
+
+    return TRUE;
 }
 
 //
@@ -125,37 +137,44 @@ BOOL InitInstance( HINSTANCE hInstance, int nCmdShow )
 //
 LRESULT CALLBACK WndProc( HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam )
 {
-	switch ( message )
-	{
-	case WM_COMMAND:
-	{
-		int wmId = LOWORD( wParam );
-		// Parse the menu selections:
-		switch ( wmId )
-		{
-		case IDM_ABOUT:
-			DialogBox( hInst, MAKEINTRESOURCE( IDD_ABOUTBOX ), hWnd, About );
-			break;
-		case IDM_EXIT:
-			DestroyWindow( hWnd );
-			break;
-		default:
-			return DefWindowProc( hWnd, message, wParam, lParam );
-		}
-	}
-	break;
-	case WM_PAINT:
-	{
-		PAINTSTRUCT ps;
-		HDC hdc = BeginPaint( hWnd, &ps );
-		// Some drawing code that uses hdc here...
+    auto logger = spdlog::get("basic_logger");
+
+    switch (message)
+    {
+    case WM_COMMAND:
+    {
+        int wmId = LOWORD( wParam );
+        logger->info("WM_COMMAND received with ID: {}", wmId);
+        // Parse the menu selections:
+        switch ( wmId )
+        {
+        case IDM_ABOUT:
+            DialogBox( hInst, MAKEINTRESOURCE( IDD_ABOUTBOX ), hWnd, About );
+            logger->info("About dialog displayed.");
+            break;
+        case IDM_EXIT:
+            DestroyWindow( hWnd );
+            logger->info("Exit command received.");
+            break;
+        default:
+            return DefWindowProc( hWnd, message, wParam, lParam );
+        }
+    }
+    break;
+    case WM_PAINT:
+    {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint( hWnd, &ps );
+        logger->info("WM_PAINT received.");
+        // Some drawing code that uses hdc here...
 		EndPaint( hWnd, &ps );
-	}
-	break;
-	case WM_DESTROY:
-		PostQuitMessage( 0 );
-		break;
-	default:
+    }
+    break;
+    case WM_DESTROY:
+        PostQuitMessage( 0 );
+        logger->info("WM_DESTROY received, posting quit message.");
+        break;
+    default:
 		return DefWindowProc( hWnd, message, wParam, lParam );
 	}
 	return 0;
